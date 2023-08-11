d49a9554fc30bd948edc9b66da754baf482f3cdf
Ravana ⭐ <47528708+ravana69@users.noreply.github.com>
2023-08-11 23:31:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,37 @@
-const express = require('express');
-const multer = require('multer');
-const { spawn } = require('child_process');
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const recordedVideo = document.getElementById('recordedVideo');
 
-const app = express();
-const port = 3000;
+let mediaRecorder;
+let recordedChunks = [];
 
-// Configure multer to handle file uploads
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-// Route to handle file upload and conversion
-app.post('/convert', upload.single('audio'), (req, res) => {
-    if (!req.file) {
-        return res.status(400).json({ error: 'No file uploaded' });
-    }
-
-    const audioBuffer = req.file.buffer;
-
-    // Convert WAV to MP3 using FFmpeg
-    const ffmpeg = spawn('ffmpeg', [
-        '-i', 'pipe:0',         // Input from pipe
-        '-f', 'mp3',            // Output format
-        'pipe:1'                // Output to pipe
-    ]);
-
-    ffmpeg.on('error', (err) => {
-        console.error('FFmpeg error:', err);
-        res.status(500).json({ error: 'FFmpeg error' });
+startButton.addEventListener('click', async () => {
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+        video: { mediaSource: 'screen' },
+        audio: true,
     });
-
-    ffmpeg.on('exit', (code) => {
-        if (code === 0) {
-            res.set('Content-Type', 'audio/mp3');
-            res.set('Content-Disposition', 'attachment; filename="converted.mp3"');
-            res.send(ffmpeg.stdout);
-        } else {
-            res.status(500).json({ error: 'Conversion error' });
+    
+    mediaRecorder = new MediaRecorder(stream);
+    
+    mediaRecorder.ondataavailable = (event) => {
+        if (event.data.size > 0) {
+            recordedChunks.push(event.data);
         }
-    });
-
-    ffmpeg.stdin.write(audioBuffer);
-    ffmpeg.stdin.end();
+    };
+    
+    mediaRecorder.onstop = () => {
+        const blob = new Blob(recordedChunks, { type: 'video/webm' });
+        recordedVideo.src = URL.createObjectURL(blob);
+    };
+    
+    mediaRecorder.start();
+    startButton.disabled = true;
+    stopButton.disabled = false;
 });
 
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+stopButton.addEventListener('click', () => {
+    mediaRecorder.stop();
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    recordedChunks = [];
 });
